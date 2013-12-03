f3dfdf9fb290c8d75d6b04da5a72560ef0b34084
fukuball <fukuball@gmail.com>
2013-12-04 05:02:39 +0800

    indent using spaces

--- iloveck101.php
+++ iloveck101.php
@@ -49,7 +49,7 @@ class ILoveCK101
 
     /**
      * Static method iloveck101
-	 *
+     *
      * @param string $url
      * @param array $options
      *
@@ -58,101 +58,101 @@ class ILoveCK101
     public static function getPictureByUrl($url, $options=array())
     {
 
-    	$defaults = array(
-         	'mode'=>'default'
-      	);
+        $defaults = array(
+            'mode'=>'default'
+        );
 
-		$options = array_merge($defaults, $options);
+        $options = array_merge($defaults, $options);
 
-      	$thread_filename = substr($url, strrpos($url, '/')+1);
+        $thread_filename = substr($url, strrpos($url, '/')+1);
 
-      	// find thread id
-		if ( !preg_match('/^thread-(\d+)-.*/', $thread_filename, $thread) ) {
+        // find thread id
+        if ( !preg_match('/^thread-(\d+)-.*/', $thread_filename, $thread) ) {
 
-			echo "URL pattern should be something like this: http://ck101.com/thread-2593278-1-1.html\n";
-   			exit;
+            echo "URL pattern should be something like this: http://ck101.com/thread-2593278-1-1.html\n";
+            exit;
 
-		}
+        }
 
-		$thread_id = $thread[1];
+        $thread_id = $thread[1];
 
-		// create `iloveck101` folder in ~/Pictures
-		$home_path = $_SERVER['HOME'];
-		$base_folder = $home_path.'/Pictures/iloveck101';
+        // create `iloveck101` folder in ~/Pictures
+        $home_path = $_SERVER['HOME'];
+        $base_folder = $home_path.'/Pictures/iloveck101';
 
-		if (!file_exists($base_folder)) {
-    		mkdir($base_folder, 0777, true);
-		}
+        if (!file_exists($base_folder)) {
+            mkdir($base_folder, 0777, true);
+        }
 
-		// fetch html and find images
-		$thread_page_available = false;
-		$thread_page_html = false;
+        // fetch html and find images
+        $thread_page_available = false;
+        $thread_page_html = false;
 
-		while ($thread_page_available == false) {
+        while ($thread_page_available == false) {
 
-			$thread_page_html = @file_get_contents($url);
-			
-			if ($thread_page_html === false) {
-				
-				echo "Retrying ...\n";
+            $thread_page_html = @file_get_contents($url);
 
-			} else {
+            if ($thread_page_html === false) {
 
-				$thread_page_available = true;
+                echo "Retrying ...\n";
 
-			} 
+            } else {
 
-		}
+                $thread_page_available = true;
 
-		$thread_page_dom = new DomDocument();
-		@$thread_page_dom->loadHTML($thread_page_html);
-		$title_tags = $thread_page_dom->getElementsByTagName('title');
-		$title = $title_tags->item(0)->nodeValue;
-		$title_parse = explode(' - ', $title);
-		$title = $title_parse[0];
+            } 
 
-		// create target folder for saving images
-    	$thread_page_folder = $home_path.'/Pictures/iloveck101/'.$thread_id.' - '.$title;
+        }
 
-    	if (!file_exists($thread_page_folder)) {
-    		mkdir($thread_page_folder, 0777, true);
-		}
+        $thread_page_dom = new DomDocument();
+        @$thread_page_dom->loadHTML($thread_page_html);
+        $title_tags = $thread_page_dom->getElementsByTagName('title');
+        $title = $title_tags->item(0)->nodeValue;
+        $title_parse = explode(' - ', $title);
+        $title = $title_parse[0];
 
-		// iterate and save images
-		$img_tags = $thread_page_dom->getElementsByTagName('img');
+        // create target folder for saving images
+        $thread_page_folder = $home_path.'/Pictures/iloveck101/'.$thread_id.' - '.$title;
 
-		foreach ($img_tags as $img_tag) {
+        if (!file_exists($thread_page_folder)) {
+            mkdir($thread_page_folder, 0777, true);
+        }
 
-			$img_src = $img_tag->getAttribute('file');
-			$img_filename = substr($img_src, strrpos($img_src, '/')+1);
-			
-			// ignore useless image
-			if (filter_var($img_src, FILTER_VALIDATE_URL) === false) {
-    			continue;
-			}
+        // iterate and save images
+        $img_tags = $thread_page_dom->getElementsByTagName('img');
 
-			// fetch image
-        	echo "Fetching $img_src ...\n";
-        	$img_src_data = @file_get_contents($img_src);
+        foreach ($img_tags as $img_tag) {
 
-        	// ignore small images
-        	$ck101_img = @imagecreatefromstring($img_src_data);
-			$ck101_img_width = @imagesx($ck101_img);
-			$ck101_img_height = @imagesy($ck101_img);
-			if ($ck101_img_width<400 || $ck101_img_height<400) {
-				continue;
-			}
+            $img_src = $img_tag->getAttribute('file');
+            $img_filename = substr($img_src, strrpos($img_src, '/')+1);
+        	
+            // ignore useless image
+            if (filter_var($img_src, FILTER_VALIDATE_URL) === false) {
+                continue;
+            }
 
-			// save image
-			$ck101_img_path = $thread_page_folder.'/'.$img_filename;
-			if (!file_exists($ck101_img_path)) {
-    			file_put_contents($ck101_img_path, $img_src_data);
-    			echo "Fetching $img_src success ...\n";
-			} else {
-				echo "$img_src already exist ...\n";
-			}	
+            // fetch image
+            echo "Fetching $img_src ...\n";
+            $img_src_data = @file_get_contents($img_src);
+
+            // ignore small images
+            $ck101_img = @imagecreatefromstring($img_src_data);
+            $ck101_img_width = @imagesx($ck101_img);
+            $ck101_img_height = @imagesy($ck101_img);
+            if ($ck101_img_width<400 || $ck101_img_height<400) {
+                continue;
+            }
+
+        	// save image
+            $ck101_img_path = $thread_page_folder.'/'.$img_filename;
+            if (!file_exists($ck101_img_path)) {
+                file_put_contents($ck101_img_path, $img_src_data);
+                echo "Fetching $img_src success ...\n";
+            } else {
+                echo "$img_src already exist ...\n";
+            }	
             
-		}		
+        }		
 
 
     }// end function iloveck101
