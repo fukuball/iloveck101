547e707ca5aba38978eae0f4a47c2f393cc35ff8
fukuball <fukuball@gmail.com>
2013-12-17 00:41:33 +0800

    travis test

---
+++ test/UtilityTest.php
@@ -0,0 +1,15 @@
+<?php
+class UtilityTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testCopyDirectory()
+    {
+        $this->assertEquals('true', 'true');
+    }
+
+    public function testDeleteDirectory()
+    {
+        $this->assertEquals('true', 'true');
+    }
+}
+?>
