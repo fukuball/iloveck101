494f362cda975a0b387fb578f0803073f5c79c88
Fukuball Lin <fukuball@gmail.com>
2013-12-17 20:05:53 +0800

    CK101Utility coverage

--- test/CK101UtilityTest.php
+++ test/CK101UtilityTest.php
@@ -24,6 +24,20 @@ class CK101UtilityTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(true, $copy_sub_folder_exist);
         $this->assertEquals(true, $copy_file2_exist);
 
+        // source is file
+        $source_folder       = dirname(__FILE__).'/test-data/test-parent-directory/images1.png';
+        $destination_folder  = dirname(__FILE__).'/test-data/copyed-directory';
+
+        $copy_options = array('mode'=>'debug');
+        CK101Utility::copy_directory($source_folder, $destination_folder, $copy_options);
+
+        $copy_file_exist = file_exists($destination_folder);
+
+        $delete_options = array('mode'=>'debug');
+        CK101Utility::delete_directory($destination_folder, $delete_options);
+
+        $this->assertEquals(true, $copy_file_exist);
+
     }
 
     public function testDeleteDirectory()
@@ -46,6 +60,20 @@ class CK101UtilityTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(false, $copy_file1_exist);
         $this->assertEquals(false, $copy_sub_folder_exist);
         $this->assertEquals(false, $copy_file2_exist);
+
+        // source is file
+        $source_folder       = dirname(__FILE__).'/test-data/test-parent-directory/images1.png';
+        $destination_folder  = dirname(__FILE__).'/test-data/copyed-directory';
+
+        $copy_options = array('mode'=>'debug');
+        CK101Utility::copy_directory($source_folder, $destination_folder, $copy_options);
+        $delete_options = array('mode'=>'debug');
+        CK101Utility::delete_directory($destination_folder, $delete_options);
+
+        $copy_file_exist = file_exists($destination_folder);
+
+        $this->assertEquals(false, $copy_file_exist);
+
     }
 }
 ?>
